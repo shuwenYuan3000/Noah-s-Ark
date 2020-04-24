e4b8b8e5e2d9cfce36b60720989331830ef2de11
shuwenYuan3000 <61443759+shuwenYuan3000@users.noreply.github.com>
2020-04-24 23:03:17 +0800

    Update noah.cpp

--- noah.cpp
+++ noah.cpp
@@ -27,25 +27,80 @@ void print_background() {
 	cout << "But for the rest of the coins other than 50, each one coin can change into a million dollars in the real world." << endl;
 }
 
+void trial() {
+	int tscore=0, rscore=0;
+	string choice;
+	cout << "----------------------------------------------" << endl;
+	cout << "In case you are not clear with the rule, Let\'s start with a little trial." << endl;
+	cout << "You now have " << tscore << " coins." << endl;
+	cout << "Your rival now have " << rscore << " coins." << endl;
+	cout << "Type \"Yes\" if you would like to cooperate, else type \"No\"." << endl;
+	cout << "You: ";
+	cin >> choice;
+	cout << "Your rival: Yes" << endl;
+	if (choice == "Yes") {
+		tscore += 2;
+		rscore += 2;
+	}
+	else {
+		tscore += 4;
+		rscore -= 1;
+	}
+	cout << "You now have " << tscore << " coins." << endl;
+	cout << "Your now have " << rscore << " coins." << endl;
+	cout << "++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+	cout << "You: ";
+	cin >> choice;
+	cout << "Your rival: No" << endl;
+	if (choice == "Yes") {
+		tscore -= 1;
+		rscore += 4;
+	}
+	cout << "You now have " << tscore << " coins." << endl;
+	cout << "Your now have " << rscore << " coins." << endl;
+	cout << "Now ended the trial. Good luck and wish you an enjoyable voyage! HAHAHAHAHAHA..." << endl;
+	cout << "----------------------------------------------" << endl;
+}
+
+void gameplay(int &score, int overall[5][5][2]) {
+	cout << "Play with 5 players and you will detemine your destiny." << endl;
+	cout << "----------------------------------------------" << endl;
+	/*
+	girl();
+	black();
+	godfather();
+	repulicate();
+	randomplay();
+	*/
+}
 
 int main() {
+	string name;
+	cout << "The young, What's your name: ";
+	cin >> name;
+	//读取文件的那部分还没写哦
+	int score = 0;
+	int overall[5][5][2];
 	print_background();
 	string start_game;
-	bool play=false;
+	bool play = false;
+	trial();
 	while (!play) {
-		cout << "Type \"Yes\" if you would like to proceed:";
-	    cin >> start_game;
-	    if (start_game == "Yes") {
-	    	play = true;
-		gameplay();
-	    } 
-	    if (start_game == "No") {
-			cout << "If you don't play the game, you will be taken for human trials right away.  Think again!" << endl;
-	    }
-	    if (start_game != "Yes" && start_game != "No"){
-	    	cout << "Invalid Choice!" << endl;
+		cout << "Type \"Yes\" if you would like to proceed:";//可能造成一点点误解，要不加个print“no”就quit这样？
+		cin >> start_game;
+		if (start_game == "Yes") {
+			play = true;
+			gameplay(score, overall);
 		}
-	} 
+		if (start_game == "No") {
+			cout << "If you don't play the game, you will be taken for human trials right away.  Think again!" << endl;
+		}
+		if (start_game != "Yes" && start_game != "No") {
+			cout << "Invalid Choice!" << endl;
+		}
+	}
+	//evaluate();
+	//file的那部分还没写哦
 
 	return 0;
 }
