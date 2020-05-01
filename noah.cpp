132289cdd9fe4145431028cbfbe93e7babe683e5
yunyurain <u3563750@connect.hku.hk>
2020-05-02 00:11:41 +0800

    simpleton & random call function

--- noah.cpp
+++ noah.cpp
@@ -23,10 +23,10 @@ void print_background() {
 	cout << "  Cooperate            +2/+2        +0/+4" << endl;
 	cout << "  Cheat                +4/+0        -1/-1" << endl;
 	cout << "----------------------------------------------" << endl;
-	cout << "Those who finally win more than 50 coins can get off the board." << endl;
+	cout << "Those who finally win more than 40 coins can get off the board." << endl;
 	cout << "\"So if in all rounds we choose Cooperate then we all can get off the board!\", the smart said."<< endl;
 	cout << endl;
-	cout << "But for the rest of the coins other than 50, each one coin can change into a million dollars in the real world." << endl;
+	cout << "But for the rest of the coins other than 40, each one coin can change into a million dollars in the real world." << endl;
 }
 
 void trial() {
@@ -77,7 +77,7 @@ void judge(int &score, int you, int npc) {
 	    score -= 1;
 }
 
-void girl(int& score, int record[5][5][2]) {
+void girl(int& score, int record[7][5][2]) {
 	cout << "----------------------------------------------" << endl;
 	cout << "There is a little girl in her pink hat." << endl;
 	cout << "With her innocent eyes filled with fear." << endl;
@@ -103,11 +103,11 @@ void girl(int& score, int record[5][5][2]) {
 		cout << "You now have " << score << " coins." << endl;
 	}
 	cout << "----------------------------------------------" << endl;
-	cout << "THE END OF ROUND 1" << endl;
+	cout << "THE END OF ROUND" << endl;
 	cout << "You now have " << score << " coins." << endl;
 }
 
-void black(int& score, int record[5][5][2]) {
+void black(int& score, int record[7][5][2]) {
 	cout << "----------------------------------------------" << endl;
 	cout << "There is a middle-aged man in tidy suit." << endl;
 	cout << "With sophisticated smile on his face." << endl;
@@ -133,11 +133,11 @@ void black(int& score, int record[5][5][2]) {
 		cout << "You now have " << score << " coins." << endl;
 	}
 	cout << "----------------------------------------------" << endl;
-	cout << "THE END OF ROUND 2" << endl;
+	cout << "THE END OF ROUND" << endl;
 	cout << "You now have " << score << " coins." << endl;
 }
 
-void godfather(int& score, int record[5][5][2]) {
+void godfather(int& score, int record[7][5][2]) {
 	cout << "----------------------------------------------" << endl;
 	cout << "There sits a godfather." << endl;
 	cout << "Cigarattee at hand." << endl;
@@ -168,11 +168,11 @@ void godfather(int& score, int record[5][5][2]) {
 		cout << "You now have " << score << " coins." << endl;
 	}
 	cout << "----------------------------------------------" << endl;
-	cout << "THE END OF ROUND 3" << endl;
+	cout << "THE END OF ROUND" << endl;
 	cout << "You now have " << score << " coins." << endl;
 }
 
-void copycat(int &score, int record[5][5][2]) {
+void copycat(int &score, int record[7][5][2]) {
 	cout << "----------------------------------------------" << endl;
 	cout << "There is a young man in a blue hat." << endl;
 	cout << "With a kind smile on his face." << endl;
@@ -184,6 +184,7 @@ void copycat(int &score, int record[5][5][2]) {
 		cout << "You: ";
 		string choice;
 		cin >> choice;
+		
 		if (i==0) {
 			cout << "Your rival: Yes" << endl;
 			if (choice == "Yes" || choice == "yes") {
@@ -220,11 +221,11 @@ void copycat(int &score, int record[5][5][2]) {
 		}
 	}
 	cout << "----------------------------------------------" << endl;
-	cout << "THE END OF ROUND 4" << endl;
+	cout << "THE END OF ROUND" << endl;
 	cout << "You now have " << score << " coins." << endl;
 }
 
-void random(int &score, int record[5][5][2]) {
+void random(int &score, int record[7][5][2]) {
 	cout << "----------------------------------------------" << endl;
 	cout << "There sits a guy in a red hat." << endl;
 	cout << "Whose facial expression is dull." << endl;
@@ -257,17 +258,107 @@ void random(int &score, int record[5][5][2]) {
 		cout << "You now have " << score << " coins." << endl;
 	}
 	cout << "----------------------------------------------" << endl;
-	cout << "THE END OF ROUND 5" << endl;
+	cout << "THE END OF ROUND" << endl;
 	cout << "You now have " << score << " coins." << endl;
 }
 
-void gameplay(int &score, int record[5][5][2]){
+void simpleton(int &score, int record[7][5][2]) {
+	cout << "----------------------------------------------" << endl;
+	cout << "Here comes a teenager." << endl;
+	cout << "Who looks simple, but not easy to deal with." << endl;
+	cout << "The principle of making decisions may be very simple for some people." << endl;
+	for (int i=0; i<5; i++) {
+		cout << "++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+		cout << "You now have " << score << " coins." << endl;
+		cout << "Type \"Yes\" if you would like to cooperate, else type \"No\"." << endl;
+		cout << "You: ";
+		string choice;
+		cin >> choice;
+		
+		if (i==0) {
+			cout << "Your rival: Yes" << endl;
+			if (choice == "Yes" || choice == "yes") {
+				judge(score, 1, 1);
+				record[5][0][0] = 1;
+				record[5][0][1] = 1;
+			}
+			else if (choice == "No" || choice == "no") {
+				judge(score, 0, 1);
+				record[5][0][0] = 0;
+				record[5][0][1] = 1;
+			}
+			cout << "You now have " << score << " coins." << endl;
+		}
+		
+		else {
+			if (record[5][i-1][0] == 1) {
+				record[5][i][1] = record[5][i-1][1];
+				if (record[5][i][1]==1) {
+						cout << "Your rival: Yes" << endl;
+				}
+				else {
+						cout << "Your rival: No" << endl;
+				}
+				if (choice == "Yes" || choice == "yes") {
+					judge(score, 1, record[5][i][1]);
+				}
+				else if (choice == "No" || choice == "no") {
+					judge(score, 0, record[5][i][1]);
+				}
+				cout << "You now have " << score << " coins." << endl;
+			}
+			else {
+				if (record[5][i-1][1]==0) {
+					record[5][i][1] = 1;
+					cout << "Your rival: Yes" << endl;
+				}
+				else {
+					record[5][i][1] = 0;
+					cout << "Your rival: No" << endl;
+				}
+				if (choice == "Yes" || choice =="yes") {
+					judge(score, 1, record[5][i][1]);
+				}
+				else if (choice == "No" || choice == "no") {
+					judge(score, 0, record[5][i][1]);
+				}
+				cout << "You now have " << score << " coins." << endl;
+			}
+		}
+	}
+	cout << "----------------------------------------------" << endl;
+	cout << "THE END OF ROUND" << endl;
+	cout << "You now have " << score << " coins." << endl;
+}
+
+void call_opponent(int opponent, int &score, int record[7][5][2]) {
+	if (opponent==1) {
+		girl(score, record);
+	}
+	else if (opponent==2) {
+		black(score, record);
+	}
+	else if (opponent==3) {
+		godfather(score, record);
+	}
+	else if (opponent==4) {
+		copycat(score, record);
+	}
+	else if (opponent==5) {
+		random(score, record);
+	}
+	else if (opponent==6) {
+		simpleton(score, record);
+	}
+}
+void gameplay(int &score, int record[7][5][2]){
 	cout << "Play with 5 players and you will detemine your destiny." << endl;
-	girl(score, record);
-	black(score, record);
-	godfather(score, record);
-	copycat(score, record);
-	random(score, record);
+	srand(time(NULL));
+	int opponent;
+	for (int j=0; j<5; j++) {
+		opponent = rand() % 6 + 1;                 // randomly call five opponents
+		call_opponent(opponent, score, record);
+	}
 }
 
 int main() {
@@ -276,7 +367,7 @@ int main() {
 	cin >> name;
 	//读取文件的那部分还没写哦
 	int score = 0;
-	int record[5][5][2];
+	int record[7][5][2];
 	print_background();
 	string start_game;
 	bool play = false;
@@ -284,14 +375,14 @@ int main() {
 	while (!play) {
 		cout << "Type \"Yes\" if you would like to proceed:";//可能造成一点点误解，要不加个print“no”就quit这样？
 		cin >> start_game;
-		if (start_game == "Yes") {
+		if (start_game == "Yes" || start_game == "yes") {
 			play = true;
 			gameplay(score, record);
 		}
-		if (start_game == "No") {
+		if (start_game == "No" || start_game == "no") {
 			cout << "If you don't play the game, you will be taken for human trials right away.  Think again!" << endl;
 		}
-		if (start_game != "Yes" && start_game != "No") {
+		if (start_game != "Yes" && start_game != "No" && start_game != "yes" && start_game != "no") {
 			cout << "Invalid Choice!" << endl;
 		}
 	}
